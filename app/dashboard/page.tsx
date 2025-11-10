7fae778172f58ab31fd0a5956430e4df5bac6a6a
rezzzarea <rezzzarea@gmail.com>
2025-11-10 10:58:05 +0700

    siap tempur

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { PageWrapper } from "@/components/wrappers/page-wrapper"
 import { auth } from "@/lib/auth"
 import { getNotebooks } from "@/server/notebook"
 import { headers } from "next/headers"
+import { Suspense } from "react"
 
 export default async function Page() {
   // menampilkan email yg terhubung dg session berdasarkan data login
@@ -16,18 +17,20 @@ export default async function Page() {
   // console.log("notebooks: ", notebooks)
   // console.log("session: ", session)
   return (
-    <PageWrapper breadcrumbs={[{label:"Dashboard",href:"/dashboard"}]}>
-      <h1>Bismillah, berikut dashboard dari user dengan email : {user?.email}</h1>
-      <CreateNotebookButton />
-      <div className="grid grid-cols-1 gap-4 md:grid-cols2 lg:grid-cols-3 xl:grid-cols-4">
-        {notebooks.success && notebooks?.notebooks?.map((notebook)=>(
-          <NotebookCard key={notebook.id} notebook={notebook}/>
-        ))}
-      </div>
-      {notebooks.success && notebooks?.notebooks?.length === 0 && (
-        <div>No notebooks found, please make your notebook</div>
-      )}
+    <Suspense>
+      <PageWrapper breadcrumbs={[{label:"Dashboard",href:"/dashboard"}]}>
+        <h1>Bismillah, berikut dashboard dari user dengan email : {user?.email}</h1>
+        <CreateNotebookButton />
+        <div className="grid grid-cols-1 gap-4 md:grid-cols2 lg:grid-cols-3 xl:grid-cols-4">
+          {notebooks.success && notebooks?.notebooks?.map((notebook)=>(
+            <NotebookCard key={notebook.id} notebook={notebook}/>
+          ))}
+        </div>
+        {notebooks.success && notebooks?.notebooks?.length === 0 && (
+          <div>No notebooks found, please make your notebook</div>
+        )}
 
-    </PageWrapper>
+      </PageWrapper>
+    </Suspense>
   )
 }

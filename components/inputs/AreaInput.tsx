e6f0e243335931aaf1062ffc4e8a67a6505dcb5e
rezzzarea <rezzzarea@gmail.com>
2025-12-24 11:08:10 +0700

    spread u/ entire interface agar tidak melakukan manual destructuring

--- components/inputs/AreaInput.tsx
+++ components/inputs/AreaInput.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ModularInput from "./ModularInput";
+import { InputProps } from "@/types/inputs";
 
-function AreaInput() {
+function AreaInput(props:InputProps) {
   return (
     <ModularInput
       className="bg-amber-700 px-4 py-1 rounded-2xl focus:ring-red-500 outline-yellow-500 focus:ring-2"
-      placeholder="Cari Wilayah"
+      {...props}
     />
   );
 }
--- components/inputs/ModularInput.tsx
+++ components/inputs/ModularInput.tsx
@@ -6,22 +6,10 @@ ketika props interface g dikasih ? maka sifatnya wajib / tidak opsional ketika d
 onChange untuk mengambil value yg berubah setelah diketik
 */
 
-
-function ModularInput({
-  placeholder,
-  className,
-  value,
-  onChange,
-}: InputProps) {
+function ModularInput(props: InputProps) {
   return (
     <>
-      <input
-        type="text"
-        value={value}
-        placeholder={placeholder}
-        className={`${className}`}
-        onChange={onChange}
-      />
+      <input type="text" {...props} />
     </>
   );
 }
